1a33c51c7e9450082cb87dd9056e27a8c21c2a7d
Garrett <pf5dev@gmail.com>
2009-12-31 17:06:27 -0700

    Libraries Added

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from pcduino.gpio import *
+from pcduino.adc import analog_read
+from pcduino.pwm import analog_write
---
+++ motortest.py
@@ -0,0 +1,13 @@
+#Test program to drive the motors
+import gpio
+#import time
+
+speed_pin_1 = 'gpio9'
+pin_forback = 'gpio11'
+pin_go = 'gpio8'
+
+def loop():
+	while True:
+		gpio.digitalWrite(speed_pin_1, 200)
+		gpio.digitalWrite(pin_forback, gpio.HIGH)
+		gpio.digitalWrite(pin_go, gpio.HIGH)
