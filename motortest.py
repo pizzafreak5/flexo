9a2572a4ed30dcccfa4e204f715fb2f5b40297a7
Garrett <pf5dev@gmail.com>
2017-04-05 14:52:32 -0600

    Fixo Flexo!

--- motortest.py
+++ motortest.py
@@ -1,13 +1,18 @@
 #Test program to drive the motors
-import gpio
+import pcduino
 #import time
 
 speed_pin_1 = 'gpio9'
 pin_forback = 'gpio11'
 pin_go = 'gpio8'
 
+
+pcduino.pin_mode(speed_pin_1, 'OUTPUT')
+pcduino.pin_mode(pin_forback, 'OUTPUT')
+pcduinp.pin_mode(pin_go, 'OUTPUT')
+
 def loop():
 	while True:
-		gpio.analogWrite(speed_pin_1, 200)
-		gpio.digitalWrite(pin_forback, gpio.HIGH)
-		gpio.digitalWrite(pin_go, gpio.HIGH)
+		pcduino.analog_swrite(speed_pin_1, 200)
+		pcduino.digital_write(pin_forback, gpio.HIGH)
+		pcduino.digital_write(pin_go, gpio.HIGH)
