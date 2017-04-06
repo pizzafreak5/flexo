d5fb48eea3a9d33ea15b0141dabb0afc0f41af22
Garrett <pf5dev@gmail.com>
2017-04-06 13:33:31 -0600

    Fixo Flexo Fetsco

--- motortest.py
+++ motortest.py
@@ -6,6 +6,8 @@ speed_pin_1 = 'gpio9'
 pin_forback = 'gpio11'
 pin_go = 'gpio8'
 
+for pin in pcduino.pins:
+        print('\nPINS:{}'.format(str(pin)))
 
 pcduino.pin_mode(speed_pin_1, 'OUTPUT')
 pcduino.pin_mode(pin_forback, 'OUTPUT')
