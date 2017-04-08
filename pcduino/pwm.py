bc8cf4ff9dcdf3e22e9d110c4bf3a8860eeb13bf
jakston <gediminas.jakstonis@ucdenver.edu>
2017-04-08 13:28:52 -0600

    test

--- pcduino/pwm.py
+++ pcduino/pwm.py
@@ -34,5 +34,5 @@ def analog_write(pin, value):
     with open(os.path.join(path, 'level', ending), 'w+') as f:
         f.write("%d\n" % map_level)
 
-    with open(os.path.join(path, 'enable', ending), 'w+') as c:
-        c.write("1\n")
+    with open(os.path.join(path, 'enable', ending), 'w+') as f:
+        f.write("1\n")
