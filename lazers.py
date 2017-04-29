c2ff868a03a74ab166973573141c9675fa478dd5
Garrett <pf5dev@gmail.com>
2017-04-29 10:56:59 -0600

    MAH LAZERS

--- lazers.py
+++ lazers.py
@@ -21,7 +21,7 @@ laser_1_addr = 0x02
 laser_2_addr = 0x04
 laser_3_addr = 0x06
 
-def initilize():
+def initialize():
         pc.pin_mode(shutdown_pin_1, 'OUTPUT')
         pc.pin_mode(shutdown_pin_2, 'OUTPUT')
         #pc.pin_mode(shutdown_pin_3, 'OUTPUT')
