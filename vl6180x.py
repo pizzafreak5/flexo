ef6181bdae3b39bc05fb95285f8bf71e62a14640
Garrett <pf5dev@gmail.com>
2017-04-29 11:29:45 -0600

    f

--- vl6180x.py
+++ vl6180x.py
@@ -305,8 +305,9 @@ class VL6180X:
             return old_address
 
         
-        self.address = new_address
+        
         self.set_register(self.__VL6180X_I2C_SLAVE_DEVICE_ADDRESS, new_address)
+        self.address = new_address
         return self.get_register(self.__VL6180X_I2C_SLAVE_DEVICE_ADDRESS)
 
     def get_distance(self):
