218038a534cc758a952a3592ed1b5ec3de20560e
Garrett <pf5dev@gmail.com>
2017-04-30 21:47:03 -0600

    pinout change 8 -> 4

---
+++ gostraight.py
@@ -0,0 +1,28 @@
+import motor_controller as mc
+import time
+
+
+mc.move_right_forward(255)
+mc.move_left_forward(255)
+
+time.sleep(8)
+
+mc.move_stop()
+
+time.sleep(4)
+
+mc.move_right_forward(255)
+mc.move_left_backward(255)
+
+time.sleep(4)
+
+mc.move_right_backward(255)
+mc.move_left_backward(255)
+
+time.sleep(4)
+
+mc.move_stop()
+
+
+
+
